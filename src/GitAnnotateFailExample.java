c6470a9306c48ee6a1f6f86bbac045dcb73fc043
gagri <grimaldi.gaia63@gmail.com>
2019-12-02 15:17:38 +0100

    Create GitAnnotateFailExample.java

---
+++ src/GitAnnotateFailExample.java
@@ -0,0 +1,10 @@
+
+public class GitAnnotateFailExample {
+	public static void printChar(String result)
+	{
+		System.out.println(result.charAt(10));
+	}
+public static void main(String [] args){
+	printChar("ciao");
+}
+}
