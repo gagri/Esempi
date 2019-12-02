f377ad3e87da404c98cd09af9830a556af527a2f
gagri <grimaldi.gaia63@gmail.com>
2019-12-02 15:18:36 +0100

    Update GitAnnotateFailExample.java
    Bug fix

--- src/GitAnnotateFailExample.java
+++ src/GitAnnotateFailExample.java
@@ -2,7 +2,9 @@
 public class GitAnnotateFailExample {
 	public static void printChar(String result)
 	{
+		if(result.length()>=10){
 		System.out.println(result.charAt(10));
+		}
 	}
 public static void main(String [] args){
 	printChar("ciao");
