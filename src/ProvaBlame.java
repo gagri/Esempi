363e0dd61807a01ab139fa8bb3ce9789de90f1d0
gagri <grimaldi.gaia63@gmail.com>
2020-01-23 12:43:31 +0100

    Update 2

--- src/ProvaBlame.java
+++ src/ProvaBlame.java
@@ -3,7 +3,7 @@ public class ProvaBlame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello hello");
+		System.err.println("hello");
 
 	}
 
