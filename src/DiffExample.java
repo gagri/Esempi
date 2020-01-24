59ac8d40e9b6c3a7ede4191a16dc9173ab735882
gagri <grimaldi.gaia63@gmail.com>
2020-01-24 15:26:58 +0100

    Create DiffExample.java

---
+++ src/DiffExample.java
@@ -0,0 +1,13 @@
+
+public class DiffExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0; 
+		System.out.println(a);
+		for(int i=0; i<10;i++)
+			System.out.println(a+i);
+
+	}
+
+}
