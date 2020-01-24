b6adf75924fe73fc645680514ddbee5aecf41fba
gagri <grimaldi.gaia63@gmail.com>
2020-01-24 15:28:28 +0100

    Update DiffExample.java

--- src/DiffExample.java
+++ src/DiffExample.java
@@ -4,7 +4,10 @@ public class DiffExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=0; 
+		if(a>0)
 		System.out.println(a);
+		else
+			a=3;
 		for(int i=0; i<10;i++)
 			System.out.println(a+i);
 
