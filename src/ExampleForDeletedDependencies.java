b9fa2380c57185e878868cd7ef314149571e7647
gagri <grimaldi.gaia63@gmail.com>
2020-01-25 14:06:03 +0100

    Create ExampleForDeletedDependencies.java

---
+++ src/ExampleForDeletedDependencies.java
@@ -0,0 +1,11 @@
+
+public class ExampleForDeletedDependencies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a=null;
+		System.out.println(a);
+
+	}
+
+}
