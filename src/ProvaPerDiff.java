1574622fb9e72b55ac238612e6a210b3f928f8c8
gagri <grimaldi.gaia63@gmail.com>
2020-01-25 07:55:43 +0100

    Update ProvaPerDiff.java

--- src/ProvaPerDiff.java
+++ src/ProvaPerDiff.java
@@ -4,8 +4,8 @@ public class ProvaPerDiff {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String a="CIAO";
-		if(a!=null)
-			System.out.println(a);
+		if(a==null)
+			System.out.println("ciao");
 	}
 
 }
