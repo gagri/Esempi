9669fe53db1972a29219c78cb426900dd4a97436
gagri <grimaldi.gaia63@gmail.com>
2020-01-25 09:23:38 +0100

    Delete and modify

--- src/ProvaPerDiff.java
+++ src/ProvaPerDiff.java
@@ -3,8 +3,8 @@ public class ProvaPerDiff {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a="ciao";
-		System.out.println(a);
+	
+		System.out.println("hello");
 	}
 
 }
