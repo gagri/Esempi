2d493b962416c00a958ce4d0d037efd8ba97e06f
gagri <grimaldi.gaia63@gmail.com>
2020-01-25 14:06:44 +0100

    Fix for ExampleForDeletedDependencies

--- src/ExampleForDeletedDependencies.java
+++ src/ExampleForDeletedDependencies.java
@@ -4,7 +4,8 @@ public class ExampleForDeletedDependencies {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String a=null;
-		System.out.println(a);
+		if(a!=null)
+			System.out.println(a);
 
 	}
 
