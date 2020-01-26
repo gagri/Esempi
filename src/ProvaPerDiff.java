a305f5699bf4261053f4db831f687aa63b9ab706
gagri <grimaldi.gaia63@gmail.com>
2020-01-26 14:11:34 +0100

    Update 17

--- src/ProvaPerDiff.java
+++ src/ProvaPerDiff.java
@@ -3,11 +3,12 @@ public class ProvaPerDiff {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			System.out.println("");
 			int a=11;
 			System.out.println("ciao");
-			int w=1;
-			int z=1;
-			int h=1;
+			int w=6;
+			int z=6;
+			int h=6;
 	}
 
 }
