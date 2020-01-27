049cbdec54ed6699db3768d52d5cf8a4cec7781e
gagri <grimaldi.gaia63@gmail.com>
2020-01-27 08:17:06 +0100

    Prova line mapping

--- src/ProvaPerDiff.java
+++ src/ProvaPerDiff.java
@@ -3,11 +3,11 @@ public class ProvaPerDiff {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("hello");
-			System.out.println("aa");
 			int w=7;
 			int h=0;
 			int z=2;
+			if(z>0)
+				System.out.println(z);
 	}
 
 }
