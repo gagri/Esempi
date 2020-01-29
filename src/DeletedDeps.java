084094879666eb3334481cc87ed938d0b200557d
gagri <grimaldi.gaia63@gmail.com>
2020-01-29 15:58:05 +0100

    Update DeletedDeps.java

--- src/DeletedDeps.java
+++ src/DeletedDeps.java
@@ -4,7 +4,8 @@ public class DeletedDeps {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stuba
 		int a=0;
-		System.out.println(a);
+		if(a>0)
+			System.out.println(a);
 
 	}
 
