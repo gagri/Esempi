f5a50fe3e0ff0b61678fa0e0cf63c1027cd74b7e
gagri <grimaldi.gaia63@gmail.com>
2020-01-30 10:47:40 +0100

    Create PerTestPDT.java

---
+++ src/PerTestPDT.java
@@ -0,0 +1,16 @@
+
+public class PerTestPDT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean ok=true;
+		int [] array=new int[10];
+		for(int i=0; i<array.length; i++){
+			array[i]=i+1;
+		}
+		System.out.println("ok");
+		System.out.println(array);
+
+	}
+
+}
